26be147415a89dd018e118cbc7ea51411f934819
Alexander Kashev <kav2k@ya.ru>
2015-01-18 02:19:22 +0100

    Version 0.7.3.4
    Surprise for those in Credits!

--- src/js/credits.js
+++ src/js/credits.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
+  chrome.runtime.getBackgroundPage(function(win) {
+    var username = win.DiFi_folderInfo[win.DiFi_inboxID].name.slice(1);
+    $("a").filter(
+      function() { return this.textContent == username}
+    ).toggleClass("glow");
+  });
+
   $("#boring").click(switch_boring);
 });
 
