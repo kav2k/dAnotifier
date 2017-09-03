7d786f8df13aa6224d348e085d3c89473c8e3b88
Alexander Kashev <kav2k@ya.ru>
2017-09-03 20:55:16 +0200

    Eliminate prefs_lib as separate file Remove some cruft

--- src/js/prefs.js
+++ src/js/prefs.js
@@ -1,7 +1,199 @@
 /* global messagesInfo, groupMessagesInfo, aggregateClasses */
 /* global wrapWarnMessage */
-/* global Preference, IntValidator, GTValidatorMinutes, GTValidatorSeconds, BoolValidator, JSONFieldsValidator, EnumValidator */
 /* exported Prefs */
+
+// A function to construct a new Preference object
+//// args:
+// key (string): key under which the preference will be stored in localStorage
+// name (string): user-friendly name for use in errors/warnings
+// unpacker (function: string -> object): function to decode localStorage-saved version
+// packer (function: object -> string): function to encode preference for storage
+// validators (array of functions): array of validator functions (see below)
+// def (object): default value assigned to the preference if in is uninitialized, reset to default or fails validation with no fallback
+//// Note: assumes that the default value passes validation
+class Preference {
+  constructor(args) {
+    this.key = args.key;
+    this.name = args.name;
+    this.pack = args.packer || JSON_Packer;
+    this.unpack = args.unpacker || JSON_Unpacker;
+    this.def = args.def;
+    this.fields = args.fields || {key: {name: args.name, validators: []}};
+
+    this.validators = [];
+    args.validators.forEach(
+      (validator) => { this.validators.push(validator); }
+    );
+
+    this.init();
+  }
+
+  validate(input) { // Check candidate value with all registered validators
+    let messages = "";
+    let status = "PASS";
+    for (let i in this.validators) {
+      const result = (this.validators[i])(input, this);
+      messages += result.message;
+      if (result.status == "FAIL") {
+        status = "FAIL";
+        break;
+      } else if (result.status == "WARN") {
+        status = ((status == "FAIL") ? "FAIL" : "WARN");
+      }
+    }
+    return {status: status, message: messages};
+  }
+
+  reset() { // Revert to default
+    this.value = this.def;
+    localStorage[this.key] = this.pack(this.value);
+  }
+
+  init() { // Load saved value or revert to default
+    if (localStorage[this.key] === undefined) { // No value saved
+      console.log("[Prefs] No value for " + this.name + " in localStorage[" + this.key + "], using default '" + this.def + "'");
+      this.reset();
+    } else { // We've got something in localStorage
+      const result = this.validate(this.unpack(localStorage[this.key]));
+
+      if (result.status == "FAIL") {  // Validation faliure on saved value
+        console.warn(result.message);
+        console.warn("[Prefs] Saved value for " + this.name + " in localStorage[" + this.key + "] failed validation! Using default '" + this.def + "'");
+        this.reset();
+      } else { // All is well
+        this.value = this.unpack(localStorage[this.key]);
+      }
+    }
+  }
+
+  set(input) {
+    const result = this.validate(input);
+
+    if (result.status == "FAIL") {
+      console.warn(result.message);
+      console.warn("[Prefs] New value '" + input + "' for '" + this.name + "' failed validation! Keeping current '" + this.value + "'");
+    } else {
+      this.value = input;
+      localStorage[this.key] = this.pack(this.value);
+    }
+    return result;
+  }
+
+  get() {
+    this.init();
+    return this.value;
+  }
+}
+
+// Standard packers/unpackers
+
+function JSON_Packer(input) { return JSON.stringify(input); }
+function JSON_Unpacker(input) { return JSON.parse(input); }
+
+// Messages' wrappers
+
+function wrapPassMessage() {
+  return {
+    status: "PASS",
+    message: ""
+  };
+}
+
+function wrapWarnMessage(text) {
+  return {
+    status: "WARN",
+    message: '<span class="pref-warn">' + text + "</span>"
+  };
+}
+
+function wrapFailMessage(text) {
+  return {
+    status: "FAIL",
+    message: '<span class="pref-fail">' + text + "</span>"
+  };
+}
+
+//// Validators
+
+// Type
+
+function IntValidator(input) {
+  if (isFinite(input) && (input == Math.round(input))) {
+    return wrapPassMessage();
+  } else {
+    return wrapFailMessage("Must be an integer");
+  }
+}
+
+function JSONFieldsValidator(input, pref) {
+  let messages = "";
+  let status = "PASS";
+
+  for (let i in pref.fields) {
+    if (input[i] === undefined) {
+      wrapFailMessage("Wrong JSON structure (user should never see this!)");
+    }
+    for (let j in pref.fields.validators) {
+      const result = (this.fields[i].validators[j])(input[i], pref);
+      messages += result.message;
+      if (result.status == "FAIL") {
+        status = "FAIL";
+        break;
+      } else if (result.status == "WARN") {
+        status = ((status == "FAIL") ? "FAIL" : "WARN");
+      }
+    }
+  }
+
+  if (status == "FAIL") {
+    return wrapFailMessage(messages);
+  } else if (status == "WARN") {
+    return wrapWarnMessage(messages);
+  } else {
+    return wrapPassMessage();
+  }
+}
+
+function BoolValidator(input) {
+  if (typeof input === "boolean") {
+    return wrapPassMessage();
+  } else {
+    return wrapFailMessage("Must be a boolean value (user should never see this!)");
+  }
+}
+
+// Comparison
+
+function EnumValidator(values) {
+  return function(input) {
+    if (values.indexOf(input) > -1) { return wrapPassMessage(); }
+    return wrapFailMessage("Value not in possible values");
+  };
+}
+
+// Special time comparisons
+// Differ only in error messages
+
+function GTValidatorSeconds(min) {
+  return function(input) {
+    if (input < min) {
+      return wrapFailMessage("Must be no less than " + min / 1000 + " second(s)");
+    } else {
+      return wrapPassMessage();
+    }
+  };
+}
+
+function GTValidatorMinutes(min) {
+  return function(input) {
+    if (input < min) {
+      return wrapFailMessage("Must be no less than " + min / 60000 + " minute(s)");
+    } else {
+      return wrapPassMessage();
+    }
+  };
+}
+
 var Prefs = {};
 
 Prefs.add = function(args) {
